49782a6f568094101ab8a99ab936d91d7d344975
fusayyun <30427256+fusayyun@users.noreply.github.com>
2021-04-13 10:38:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,45 @@
+// move background start
 var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
+		lFollowY = 0,
+		x = 0,
+		y = 0,
+		friction = 1 / 30;
 
 function moveBackground() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-  
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+	x += (lFollowX - x) * friction;
+	y += (lFollowY - y) * friction;
+	
+	translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
 
-  $('.bg').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
+	$('.bg').css({
+		'-webit-transform': translate,
+		'-moz-transform': translate,
+		'transform': translate
+	});
 
-  window.requestAnimationFrame(moveBackground);
+	window.requestAnimationFrame(moveBackground);
 }
 
 $(window).on('mousemove click', function(e) {
 
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
+	var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+	var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+	lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+	lFollowY = (10 * lMouseY) / 100;
 
 });
 
 moveBackground();
-function random(num) {
-  return Math.floor(Math.random()*num)
-}
-
-function getRandomStyles() {
-  var r = random(255);
-  var g = random(255);
-  var b = random(255);
-  var mt = random(200);
-  var ml = random(50);
-  var dur = random(5)+5;
-  return `
-  background-color: rgba(${r},${g},${b},0.7);
-  color: rgba(${r},${g},${b},0.7); 
-  box-shadow: inset -7px -3px 10px rgba(${r-10},${g-10},${b-10},0.7);
-  margin: ${mt}px 0 0 ${ml}px;
-  animation: float ${dur}s ease-in infinite
-  `
-}
-
-function createBalloons(num) {
-  var balloonContainer = document.getElementById("balloon-container")
-  for (var i = num; i > 0; i--) {
-    var balloon = document.createElement("div");
-    balloon.className = "balloon";
-    balloon.style.cssText = getRandomStyles();           
-    balloonContainer.append(balloon);
-  }
-}
-
+// move background end
+// 
 window.onload = function() {
-  createBalloons(15);
-  var timeoutID = window.setTimeout(( function(){
-    $("#balloon-container").hide()
-  }), 5500);
+	// $('site-navbar').animate({
+	// 	left: '0' }, 500, function() {
+	// 	// $('ul li').delay(900).fadeIn(200)
+	// 	$('ul li').delay(500).each(function(index) {
+	// 		$(this).delay(200*index).fadeIn(200);
+	// 	}); 
+	// });
+	var timeoutID = window.setTimeout(( function(){
+		$("#bubble-container").hide()}), 3000);
 }
